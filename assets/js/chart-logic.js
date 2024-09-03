da5df8a8fd82c44edac37074c200ed7ee7da8d65
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-09-03 16:35:12 +0500

    Task Completed

--- assets/js/chart-logic.js
+++ assets/js/chart-logic.js
@@ -1,141 +1,75 @@
-// Chart Data
-const firstHalfBarsItemStyle = { color: "#5f89f0", borderColor: "#5f89f0" };
-const team_chartData = {
-  chartBarTitle: "Value: ",
-  baselineBarTitle: "Baseline Title",
-  barData: [
-    { value: 80, itemStyle: firstHalfBarsItemStyle },
-    { value: 100, itemStyle: firstHalfBarsItemStyle },
-    { value: 130, itemStyle: firstHalfBarsItemStyle },
-    { value: 190, itemStyle: firstHalfBarsItemStyle },
-    { value: 260, itemStyle: firstHalfBarsItemStyle },
-    { value: 220, itemStyle: firstHalfBarsItemStyle },
-    { value: 300, itemStyle: firstHalfBarsItemStyle },
-  ],
-  labels: [
-    "Jan 2024",
-    "Feb 2024",
-    "Mar 2024",
-    "Apr 2024",
-    "May 2024",
-    "Jun 2024",
-    "Jul 2024",
-  ],
-};
+const Chart_data = [
+  {
+    value: 70,
+    year: 2018,
+    popupTitle: "Ex-ML Researchers",
+    popupDesc:
+      "Founded by ex-ML researchers from Google, OpenAl, & U.S.Department",
+  },
+  {
+    value: 80,
+    year: 2019,
+    popupTitle: "Ex-ML Researchers",
+    popupDesc:
+      "Founded by ex-ML researchers from Google, OpenAl, & U.S.Department",
+  },
+  {
+    value: 90,
+    year: 2020,
+    popupTitle: "Ex-ML Researchers",
+    popupDesc:
+      "Founded by ex-ML researchers from Google, OpenAl, & U.S.Department",
+  },
+  {
+    value: 100,
+    year: 2021,
+    popupTitle: "Ex-ML Researchers",
+    popupDesc:
+      "Founded by ex-ML researchers from Google, OpenAl, & U.S.Department",
+  },
+  {
+    value: 110,
+    year: 2022,
+    popupTitle: "Ex-ML Researchers",
+    popupDesc:
+      "Founded by ex-ML researchers from Google, OpenAl, & U.S.Department",
+  },
+  {
+    value: 120,
+    year: 2023,
+    popupTitle: "Ex-ML Researchers",
+    popupDesc:
+      "Founded by ex-ML researchers from Google, OpenAl, & U.S.Department",
+  },
+];
 
-// Chart Functionalities
-const chartElem = document.getElementById("chart");
+const chart = document.getElementById("chart");
 
-let chartData = team_chartData;
+//   Finding the highest value
+const maxValue = Chart_data.reduce(
+  (max, current) => (current.value > max ? current.value : max),
+  Chart_data[0].value
+);
 
-let chart = echarts.init(chartElem, "light", {
-  renderer: "svg",
-  useDirtyRect: false,
+console.log("The highest value is:", maxValue);
+Chart_data.forEach((itemData) => {
+  const heightPer = (itemData.value / maxValue) * 100;
+  const ItemHTML = `
+              <div class="chart_item">
+                <div class="item-body" style="--height:${heightPer}%;">
+                  <div class="popup">
+                    <h2>${itemData.popupTitle}</h2>
+                    <p>
+                      ${itemData.popupDesc}
+                    </p>
+                    <span class="gradient-color" >
+                      ${itemData.value}%
+                    </span>
+                  </div> 
+                  <div class="text">${itemData.year}</div>
+                </div>
+              </div>
+  `;
+
+  chart.innerHTML += ItemHTML;
 });
-
-let option = {
-  tooltip: {
-    trigger: "axis",
-    axisPointer: {
-      type: "shadow",
-    },
-    formatter: function (params) {
-      var content = "";
-      params.forEach(function (param) {
-        content += "<h1>" + param.value + "</h1>";
-      });
-      return content;
-    },
-  },
-  legend: {
-    show: false,
-  },
-  toolbox: {
-    show: false,
-  },
-  xAxis: [
-    {
-      show: true,
-      type: "category",
-      axisLine: { show: true },
-      splitLine: {
-        show: false,
-        lineStyle: {
-          type: "dashed",
-        },
-      },
-      axisTick: { show: true },
-      axisLabel: {
-        show: true,
-        textStyle: {
-          color: "black",
-          fontSize: 24,
-        },
-      },
-      interval: 1,
-
-      data: chartData.labels,
-
-      boundaryGap: true,
-    },
-    {
-      show: false,
-      splitLine: { show: false },
-      axisTick: { show: false },
-      axisLabel: { show: false },
-      type: "value",
-      axisPointer: {
-        show: false, // Disable hover effect on the x-axis
-      },
-      data: chartData.labels,
-      boundaryGap: false,
-    },
-  ],
-  yAxis: [
-    {
-      type: "value",
-      splitLine: {
-        show: false,
-      },
-      axisLine: {
-        show: false,
-      },
-      axisLabel: {
-        show: false,
-      },
-      min: 0,
-      axisTick: { show: false },
-    },
-  ],
-
-  grid: {
-    left: "0%",
-    top: "10%",
-    right: "0%",
-    width: "100%",
-    height: "89%",
-    bottom: "0%",
-    containLabel: true,
-  },
-  series: [
-    {
-      xAxisIndex: 0,
-      name: chartData.chartBarTitle,
-      type: "bar",
-      barMaxWidth: 250,
-      label: {
-        show: false,
-        fontSize: 24,
-        position: "top",
-        color: "#6C82FA",
-      },
-      itemStyle: {
-        borderRadius: [4, 4, 0, 0],
-      },
-
-      data: chartData.barData,
-    },
-  ],
-};
-
-chart.setOption(option);
