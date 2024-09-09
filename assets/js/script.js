5015951ad607e2d1a5d5f8c0d8fb32021fa25eb3
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-09-10 00:54:12 +0500

    Menu Added!

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,3 +185,58 @@ tabBtns.forEach((tab, i) => {
     tabImgs[i].classList.add("active");
   });
 });
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// Declaring the constants
+
+let allNavLinks = [];
+function createNavLinks() {
+  allNavLinks = [];
+  for (let i = 0; i < 8; i++) {
+    allNavLinks[i] = document.getElementById(`nav-link${i + 1}`);
+  }
+}
+createNavLinks();
+
+const nav = document.getElementById("nav");
+const menuBar = document.getElementById("menuBar");
+
+// Now making a function to remove classes as well as add in the acher tag to perform animations
+function addRemoveClasses(action) {
+  // We have to add classes as well as remove from all the acher tag, so we will use map methode of allNavLinks array to call its all elements and perform functions with all.
+
+  allNavLinks.map((curElem, i) => {
+    // Here we are using string letrals `` back tiks on the conditional base of our parameter which will be passed when called.
+    curElem.classList.add(`slide-${action === "in" ? "in" : "out"}-${i + 1}`);
+    curElem.classList.remove(
+      `slide-${action === "in" ? "out" : "in"}-${i + 1}`
+    );
+  });
+}
+
+// Defining Functions
+function toggleNav() {
+  // toggling the menuBar button
+  menuBar.classList.toggle("change");
+  nav.classList.toggle("swipe-nav");
+  // Now we are calling the class exchanging function on the conditonal base.
+  menuBar.classList.contains("change")
+    ? addRemoveClasses("in")
+    : addRemoveClasses("out");
+}
+
+// Event listeners
+menuBar.addEventListener("click", toggleNav);
+// We are also using map function so that we should not to write again  and again.
+allNavLinks.map((curElem, i) => {
+  curElem.addEventListener("click", () => {
+    toggleNav();
+    sections.forEach((section) => {
+      // Remove the 'active' class from each section
+      section.classList.remove("active");
+    });
+    sections[i].classList.add("active");
+    activeSectionIndex = i;
+  });
+});
